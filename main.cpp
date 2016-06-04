6850a272e19d12047c7852ba4a753b0b9759b017
ace-dexter <barnwal.skbjarvis@gmail.com>
2016-06-04 10:24:56 +0530

    Cplus plus added

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class skbClass
+{
+	public:
+		skbClass(string z)
+		{
+			setName(z);
+		}
+		void setName(string x)
+		{
+			name = x;
+		}
+		string getName()
+		{
+			return name;
+		}
+	private:
+		string name;
+};
+
+int main()
+{
+	skbClass bo("Hello world...");
+	cout << bo.getName();
+	
+	bo.setName("sandy barton");	
+	cout << bo.getName() << endl;
+	
+	skbClass bo2("sally salad yeah!!!");
+	cout << bo2.getName()<<endl;
+	return 0;
+}
